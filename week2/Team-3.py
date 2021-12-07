1811d7c19b997a9b9ecc14b751a11f00b6e840c6
PC <nafay2002rizwani@gmail.com>
2021-12-08 02:04:41 +0500

    BFS AND DFS

--- week2/Team-3.py
+++ week2/Team-3.py
@@ -27,14 +27,39 @@ class Graph:
                 return False
         return True
 
-    def print_as_grid(graph, n): #print Graph as grid n = row size and column size
+    def print_as_grid(graph, n):  # print Graph as grid n = row size and column size
         for i in range(n):
             for j in range(n):
                 print(graph.Matrix[i][j], end=" ")
             print("\n")
 
+    def BFS_traversal(self, start):
+        # BFS traversal of the graph
+        visited = [False] * self.n
+        queue = []
+        queue.append(start)
+        visited[start] = True
+        while queue:
+            start = queue.pop(0)
+            print(start, end=" ")
+            for i in range(self.n):
+                if self.Matrix[start][i] == 1 and visited[i] == False:
+                    queue.append(i)
+                    visited[i] = True
 
-
+    def DFS_traversal(self, start):
+        # DFS traversal of the graph
+        visited = [False] * self.n
+        stack = []
+        stack.append(start)
+        visited[start] = True
+        while stack:
+            start = stack.pop()
+            print(start, end=" ")
+            for i in range(self.n):
+                if self.Matrix[start][i] == 1 and visited[i] == False:
+                    stack.append(i)
+                    visited[i] = True
 
 empty_graph = [[0, 0, 0, 0],
                [0, 0, 0, 0],
@@ -56,6 +81,10 @@ grid_3_3 = [[0, 1, 0, 1, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 1, 0, 1, 0, 1],
             [0, 0, 0, 0, 0, 1, 0, 1, 0]]
 
-if __name__ == "__main__":
-    graph = Graph(grid_3_3)
-    print(graph.Degree(0))
+
+
+#
+# if __name__ == "__main__":
+#     # graph = Graph(grid_3_3)
+#     # print(graph.Degree(0))
+#     # graph.BFS_traversal(0)
